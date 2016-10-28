5433b434b7cd3e5892e5d28423882e6373ef39fc
ch-andrei <im.andrei@hotmail.ca>
2016-10-28 13:29:42 -0400

    Moved stuff

---
+++ lab2/grid_4_4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+#include <mpi.h>
+
+
+
+// Note: the value of the "eta" constant should be 2e-4, the value of the "rho" constant should be 0.5, and the value of the "G" constant should be 0.75.
